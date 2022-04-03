32e8880e2632bd5af5e76ea0f93abc63e0fea341
wshangold24 <89747248+wshangold24@users.noreply.github.com>
2022-04-03 13:50:36 -0500

    created player object

---
+++ src/player/Player.java
@@ -0,0 +1,16 @@
+package player;
+
+import java.util.ArrayList;
+
+public class Player {
+	
+	private ArrayList<Integer> deck;
+	private String name;
+	
+	public Player(String name) {
+		this.deck = new ArrayList<Integer>();
+		this.name = name;
+	}
+	
+	
+}
