8a940ecb8d6998bd73167b446f1bec735c4600a5
wshangold24 <89747248+wshangold24@users.noreply.github.com>
2022-04-03 12:56:48 -0500

    created card class

---
+++ src/createCard/Card.java
@@ -0,0 +1,14 @@
+package createCard;
+
+public class Card {
+
+	private int value;
+	private String suit;
+	
+	void Card(int value, String suit) {
+		value = this.value;
+		suit = this.suit;
+	}
+	
+	
+}
