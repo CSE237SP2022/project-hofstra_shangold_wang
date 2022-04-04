5c15d7578dc17b583d536b629000b1542f0ac783
wshangold24 <89747248+wshangold24@users.noreply.github.com>
2022-04-04 13:57:13 -0500

    added points methods/tests

--- src/player/Player.java
+++ src/player/Player.java
@@ -6,10 +6,12 @@ public class Player {
 	
 	public ArrayList<Integer> deck;
 	public String name;
+	public int points;
 	
 	public Player(String name) {
 		this.deck = new ArrayList<Integer>();
 		this.name = name;
+		this.points = 0;
 	}
 	
 	public ArrayList<Integer> getDeck() {
@@ -20,5 +22,16 @@ public class Player {
 		return this.name;
 	}
 	
+	public int getPoints() {
+		return this.points;
+	}
+	
+	public void addPoints(int points) {
+		this.points += points;
+	}
+	
+	public void resetPoints() {
+		this.points = 0;
+	}
 	
 }
