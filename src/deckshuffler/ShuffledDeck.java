aee0fb0e5ca76cc98888219bd1ccd4661eb7586f
rhofstra16 <rhofstra@wustl.edu>
2022-04-04 13:15:12 -0500

    added method for final output that includes array list of lists that shuffles deck into player decks and main deck.

--- src/deckshuffler/ShuffledDeck.java
+++ src/deckshuffler/ShuffledDeck.java
@@ -5,11 +5,15 @@ public class ShuffledDeck {
 	private int numberOfSuit; 
 	private ArrayList<Integer> notShuffledDeck;
 	private ArrayList<Integer> shuffledDeck;
+	private ArrayList<Integer> playerDeck;
+	private ArrayList<ArrayList<Integer>> fullDeck;
 	
 	public ShuffledDeck() {
 		this.numberOfSuit = 13;
 		this.notShuffledDeck = new ArrayList<Integer>();
 		this.shuffledDeck= new ArrayList<Integer>();
+		this.playerDeck= new ArrayList<Integer>();
+		this.fullDeck= new ArrayList<ArrayList<Integer>>();
 	}
 	
 	
@@ -34,14 +38,42 @@ public class ShuffledDeck {
 	        }
 		return this.shuffledDeck;
 	}
+	public ArrayList<Integer> addShuffledCardsIntoPlayerArray(){
+		ArrayList<Integer> playerDeck = new ArrayList<Integer>();
+		
+		for(int j=0; j<7; j++) {
+				playerDeck.add(this.shuffledDeck.get(0));
+				this.shuffledDeck.remove(0);
+		}
+			
+		return playerDeck;
+	}
+	
+	public ArrayList<ArrayList<Integer>> initializeDecks(int numberPlayers) {
+	
+		for (int i = 0; i< numberPlayers; i++) {
+			
+				this.fullDeck.add(addShuffledCardsIntoPlayerArray());
+				
+			}
+		
+			this.fullDeck.add(this.shuffledDeck);
+		return this.fullDeck;
+	}
 	
 	public static void main(String[] args) {
 		ShuffledDeck deck = new ShuffledDeck();
 		ArrayList<Integer> unshuffledDeck = deck.addCardsIntoArray();
 		ArrayList<Integer> shuffledDeck = deck.addShuffledCardsIntoArray();
+		//System.out.println(shuffledDeck);
+		ArrayList<Integer> playerDeck = deck.addShuffledCardsIntoPlayerArray();
+		
+		//initializeDecks initial = new initializeDecks(4);
+		ArrayList<ArrayList<Integer>> fullDeck = deck.initializeDecks(4);
 	
-		System.out.println(shuffledDeck);
 
+		//System.out.println(playerDeck);
+		System.out.println(fullDeck);
 	}
 	
 
